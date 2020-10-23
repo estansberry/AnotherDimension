bde8892d38f31bc50c406b043a1ccbf593e8ade5
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-10-23 17:21:13 -0500

    I have tested this and it works

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,6 +1,17 @@
 public class ArrayOps{
   public static void main(String[] args) {
   }
+  public static String arrToString(int[] arr){
+    String newstr = "{";
+    if (arr.length == 0){
+      return("{}");
+    }
+    for (int i = 0; i < arr.length - 1; i ++){
+      newstr = newstr + arr[i];
+      newstr = newstr + ", ";
+    }newstr = newstr + arr[arr.length - 1] + "}";
+      return(newstr);
+  }
   public static int sum(int[] arr){
     int sum = 0;
     for (int i = 0; i < arr.length; i ++){
@@ -14,5 +25,21 @@ public class ArrayOps{
         large = arr[i];
       }
     }return(large);
+  }
+  public static int[] sumRows(int[][] matrix){
+    int[] newarr;
+    newarr = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i ++){
+      newarr[i] = sum(matrix[i]);
+    }System.out.println(arrToString(newarr));
+    return(newarr);
+  }
+  public static int[] largestInRows(int[][] matrix){
+    int[] newarr;
+    newarr = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i ++){
+      newarr[i] = largest(matrix[i]);
+    }System.out.println(arrToString(newarr));
+    return(newarr);
   }
 }
--- Tester.java
+++ Tester.java
@@ -6,6 +6,10 @@ public class Tester{
     int[] d = {-11, - 45, -13, -3};
     int[] e = {1, 2, 150, -40};
     int[] f = {0, 0};
+    int[][] g = {{5, 4, 3}, {0}, {3, 2, 2, 2, 1}, {}, {7, 9}};
+    int[][] h = {{0}};
+    int[][] i = {{5, 4, 3}, {0}, {3, 2, 2, 2, 1}, {-3}, {7, 9}};
+    int[][] j = {{0}};
     System.out.print("Should be 10: ");
     System.out.println(ArrayOps.sum(a));
     System.out.print("Should be 1: ");
@@ -18,5 +22,14 @@ public class Tester{
     System.out.println(ArrayOps.largest(e));
     System.out.print("Should be 0: ");
     System.out.println(ArrayOps.largest(f));
+    System.out.print("Should be {12, 0, 10, 0, 16}: ");
+    ArrayOps.sumRows(g);
+    System.out.print("Should be {0}: ");
+    ArrayOps.sumRows(h);
+    System.out.print("Should be {5, 0, 3, -3, 9}: ");
+    ArrayOps.largestInRows(i);
+    System.out.print("Should be {0}: ");
+    ArrayOps.largestInRows(j);
+
   }
 }
