2fb487ac2a9b11ce76fe54529c938644ff9750e9
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-10-23 12:18:39 -0500

    Done with the second method

--- ArrayOps.java
+++ ArrayOps.java
@@ -7,4 +7,12 @@ public class ArrayOps{
       sum = sum + arr[i];
     }return(sum);
   }
+  public static int largest(int[] arr){
+    int large = -2147483648;
+    for (int i = 0; i < arr.length; i ++){
+      if (arr[i] > large){
+        large = arr[i];
+      }
+    }return(large);
+  }
 }
--- Tester.java
+++ Tester.java
@@ -3,11 +3,20 @@ public class Tester{
     int[] a = {5, 4, 1};
     int[] b = {1, 0, 0, 0};
     int[] c = {};
+    int[] d = {-11, - 45, -13, -3};
+    int[] e = {1, 2, 150, -40};
+    int[] f = {0, 0};
     System.out.print("Should be 10: ");
     System.out.println(ArrayOps.sum(a));
     System.out.print("Should be 1: ");
     System.out.println(ArrayOps.sum(b));
     System.out.print("Should be 0: ");
     System.out.println(ArrayOps.sum(c));
+    System.out.print("Should be -3: ");
+    System.out.println(ArrayOps.largest(d));
+    System.out.print("Should be 150: ");
+    System.out.println(ArrayOps.largest(e));
+    System.out.print("Should be 0: ");
+    System.out.println(ArrayOps.largest(f));
   }
 }
